e4e68bdd8a26e49fbd73c2416583f8a8f7bf2bea
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-11 22:16:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,7 +2,7 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 say_hello("Kenny")
-def say_hello(name = "Ruby Programer")
+def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
 end
 say_hello("Kenny")
